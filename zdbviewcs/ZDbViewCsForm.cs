cf3eeaa1b9066ba89a1f37f896ff0ee2eb567554
zyl910hero <zyl910hero@gmail.com>
2015-07-01 15:57:51 +0800

    创建项目.

---
+++ zdbviewcs/ZDbViewCsForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace zdbviewcs {
+	/// <summary>
+	/// zyl910's Database viewer (C#) (C#版数据库查看器).
+	/// </summary>
+	public partial class ZDbViewCsForm : Form {
+		public ZDbViewCsForm() {
+			InitializeComponent();
+		}
+	}
+}
