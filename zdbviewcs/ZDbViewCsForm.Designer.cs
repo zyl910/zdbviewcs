18cdd5344e6149f4ab6771c2cc3677d241f22f56
zyl910hero <zyl910hero@gmail.com>
2015-07-08 16:18:02 +0800

    打开数据库.

--- zdbviewcs/ZDbViewCsForm.Designer.cs
+++ zdbviewcs/ZDbViewCsForm.Designer.cs
@@ -25,33 +25,41 @@
 		private void InitializeComponent() {
 			this.components = new System.ComponentModel.Container();
 			this.spcTool = new System.Windows.Forms.SplitContainer();
-			this.pnlTool = new System.Windows.Forms.Panel();
-			this.lblProvider = new System.Windows.Forms.Label();
-			this.cboProvider = new System.Windows.Forms.ComboBox();
-			this.mnuPopup = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.mnuPopupProviderRefresh = new System.Windows.Forms.ToolStripMenuItem();
 			this.tbcInput = new System.Windows.Forms.TabControl();
 			this.tbpConnstr = new System.Windows.Forms.TabPage();
-			this.tbpSql = new System.Windows.Forms.TabPage();
 			this.txtConnstr = new System.Windows.Forms.TextBox();
+			this.tbpSql = new System.Windows.Forms.TabPage();
 			this.txtSql = new System.Windows.Forms.TextBox();
+			this.pnlTool = new System.Windows.Forms.Panel();
+			this.cboProvider = new System.Windows.Forms.ComboBox();
+			this.lblProvider = new System.Windows.Forms.Label();
+			this.mnuPopup = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.mnuPopupProviderRefresh = new System.Windows.Forms.ToolStripMenuItem();
 			this.spcList = new System.Windows.Forms.SplitContainer();
 			this.tbcList = new System.Windows.Forms.TabControl();
 			this.tbpList = new System.Windows.Forms.TabPage();
 			this.tbpListLog = new System.Windows.Forms.TabPage();
+			this.txtListLog = new System.Windows.Forms.TextBox();
 			this.tbcGrid = new System.Windows.Forms.TabControl();
 			this.tbpGrid = new System.Windows.Forms.TabPage();
 			this.tbpGridLog = new System.Windows.Forms.TabPage();
-			this.txtListLog = new System.Windows.Forms.TextBox();
 			this.txtGridLog = new System.Windows.Forms.TextBox();
+			this.tbpLog = new System.Windows.Forms.TabPage();
+			this.txtLog = new System.Windows.Forms.TextBox();
+			this.cboClickCmd = new System.Windows.Forms.ComboBox();
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.btnClose = new System.Windows.Forms.Button();
+			this.txtLimit = new System.Windows.Forms.TextBox();
+			this.btnExec = new System.Windows.Forms.Button();
+			this.ttpMain = new System.Windows.Forms.ToolTip(this.components);
 			this.spcTool.Panel1.SuspendLayout();
 			this.spcTool.Panel2.SuspendLayout();
 			this.spcTool.SuspendLayout();
-			this.pnlTool.SuspendLayout();
-			this.mnuPopup.SuspendLayout();
 			this.tbcInput.SuspendLayout();
 			this.tbpConnstr.SuspendLayout();
 			this.tbpSql.SuspendLayout();
+			this.pnlTool.SuspendLayout();
+			this.mnuPopup.SuspendLayout();
 			this.spcList.Panel1.SuspendLayout();
 			this.spcList.Panel2.SuspendLayout();
 			this.spcList.SuspendLayout();
@@ -59,6 +67,7 @@
 			this.tbpListLog.SuspendLayout();
 			this.tbcGrid.SuspendLayout();
 			this.tbpGridLog.SuspendLayout();
+			this.tbpLog.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// spcTool
@@ -81,15 +90,80 @@
 			this.spcTool.SplitterDistance = 100;
 			this.spcTool.TabIndex = 0;
 			// 
+			// tbcInput
+			// 
+			this.tbcInput.Controls.Add(this.tbpConnstr);
+			this.tbcInput.Controls.Add(this.tbpSql);
+			this.tbcInput.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tbcInput.Location = new System.Drawing.Point(0, 28);
+			this.tbcInput.Name = "tbcInput";
+			this.tbcInput.SelectedIndex = 0;
+			this.tbcInput.Size = new System.Drawing.Size(584, 72);
+			this.tbcInput.TabIndex = 1;
+			// 
+			// tbpConnstr
+			// 
+			this.tbpConnstr.Controls.Add(this.txtConnstr);
+			this.tbpConnstr.Location = new System.Drawing.Point(4, 22);
+			this.tbpConnstr.Name = "tbpConnstr";
+			this.tbpConnstr.Padding = new System.Windows.Forms.Padding(3);
+			this.tbpConnstr.Size = new System.Drawing.Size(576, 46);
+			this.tbpConnstr.TabIndex = 0;
+			this.tbpConnstr.Text = "ConnectionString";
+			this.tbpConnstr.UseVisualStyleBackColor = true;
+			// 
+			// txtConnstr
+			// 
+			this.txtConnstr.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtConnstr.Location = new System.Drawing.Point(3, 3);
+			this.txtConnstr.Multiline = true;
+			this.txtConnstr.Name = "txtConnstr";
+			this.txtConnstr.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtConnstr.Size = new System.Drawing.Size(570, 40);
+			this.txtConnstr.TabIndex = 0;
+			// 
+			// tbpSql
+			// 
+			this.tbpSql.Controls.Add(this.txtSql);
+			this.tbpSql.Location = new System.Drawing.Point(4, 22);
+			this.tbpSql.Name = "tbpSql";
+			this.tbpSql.Padding = new System.Windows.Forms.Padding(3);
+			this.tbpSql.Size = new System.Drawing.Size(576, 34);
+			this.tbpSql.TabIndex = 1;
+			this.tbpSql.Text = "SQL";
+			this.tbpSql.UseVisualStyleBackColor = true;
+			// 
+			// txtSql
+			// 
+			this.txtSql.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtSql.Location = new System.Drawing.Point(3, 3);
+			this.txtSql.Multiline = true;
+			this.txtSql.Name = "txtSql";
+			this.txtSql.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtSql.Size = new System.Drawing.Size(570, 28);
+			this.txtSql.TabIndex = 1;
+			// 
 			// pnlTool
 			// 
+			this.pnlTool.Controls.Add(this.btnExec);
+			this.pnlTool.Controls.Add(this.txtLimit);
+			this.pnlTool.Controls.Add(this.btnClose);
+			this.pnlTool.Controls.Add(this.btnOpen);
 			this.pnlTool.Controls.Add(this.cboProvider);
 			this.pnlTool.Controls.Add(this.lblProvider);
 			this.pnlTool.Dock = System.Windows.Forms.DockStyle.Top;
 			this.pnlTool.Location = new System.Drawing.Point(0, 0);
 			this.pnlTool.Name = "pnlTool";
-			this.pnlTool.Size = new System.Drawing.Size(584, 40);
+			this.pnlTool.Size = new System.Drawing.Size(584, 28);
 			this.pnlTool.TabIndex = 0;
+			// 
+			// cboProvider
+			// 
+			this.cboProvider.FormattingEnabled = true;
+			this.cboProvider.Location = new System.Drawing.Point(68, 3);
+			this.cboProvider.Name = "cboProvider";
+			this.cboProvider.Size = new System.Drawing.Size(240, 20);
+			this.cboProvider.TabIndex = 1;
 			// 
 			// lblProvider
 			// 
@@ -100,14 +174,6 @@
 			this.lblProvider.Size = new System.Drawing.Size(59, 12);
 			this.lblProvider.TabIndex = 0;
 			this.lblProvider.Text = "&Provider:";
-			// 
-			// cboProvider
-			// 
-			this.cboProvider.FormattingEnabled = true;
-			this.cboProvider.Location = new System.Drawing.Point(68, 3);
-			this.cboProvider.Name = "cboProvider";
-			this.cboProvider.Size = new System.Drawing.Size(240, 20);
-			this.cboProvider.TabIndex = 1;
 			// 
 			// mnuPopup
 			// 
@@ -123,59 +189,6 @@
 			this.mnuPopupProviderRefresh.Text = "Provider refresh";
 			this.mnuPopupProviderRefresh.Click += new System.EventHandler(this.mnuPopupProviderRefresh_Click);
 			// 
-			// tbcInput
-			// 
-			this.tbcInput.Controls.Add(this.tbpConnstr);
-			this.tbcInput.Controls.Add(this.tbpSql);
-			this.tbcInput.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tbcInput.Location = new System.Drawing.Point(0, 40);
-			this.tbcInput.Name = "tbcInput";
-			this.tbcInput.SelectedIndex = 0;
-			this.tbcInput.Size = new System.Drawing.Size(584, 60);
-			this.tbcInput.TabIndex = 1;
-			// 
-			// tbpConnstr
-			// 
-			this.tbpConnstr.Controls.Add(this.txtConnstr);
-			this.tbpConnstr.Location = new System.Drawing.Point(4, 22);
-			this.tbpConnstr.Name = "tbpConnstr";
-			this.tbpConnstr.Padding = new System.Windows.Forms.Padding(3);
-			this.tbpConnstr.Size = new System.Drawing.Size(576, 34);
-			this.tbpConnstr.TabIndex = 0;
-			this.tbpConnstr.Text = "ConnectionString";
-			this.tbpConnstr.UseVisualStyleBackColor = true;
-			// 
-			// tbpSql
-			// 
-			this.tbpSql.Controls.Add(this.txtSql);
-			this.tbpSql.Location = new System.Drawing.Point(4, 22);
-			this.tbpSql.Name = "tbpSql";
-			this.tbpSql.Padding = new System.Windows.Forms.Padding(3);
-			this.tbpSql.Size = new System.Drawing.Size(576, 34);
-			this.tbpSql.TabIndex = 1;
-			this.tbpSql.Text = "SQL";
-			this.tbpSql.UseVisualStyleBackColor = true;
-			// 
-			// txtConnstr
-			// 
-			this.txtConnstr.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.txtConnstr.Location = new System.Drawing.Point(3, 3);
-			this.txtConnstr.Multiline = true;
-			this.txtConnstr.Name = "txtConnstr";
-			this.txtConnstr.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.txtConnstr.Size = new System.Drawing.Size(570, 28);
-			this.txtConnstr.TabIndex = 0;
-			// 
-			// txtSql
-			// 
-			this.txtSql.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.txtSql.Location = new System.Drawing.Point(3, 3);
-			this.txtSql.Multiline = true;
-			this.txtSql.Name = "txtSql";
-			this.txtSql.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.txtSql.Size = new System.Drawing.Size(570, 28);
-			this.txtSql.TabIndex = 1;
-			// 
 			// spcList
 			// 
 			this.spcList.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -185,6 +198,7 @@
 			// 
 			// spcList.Panel1
 			// 
+			this.spcList.Panel1.Controls.Add(this.cboClickCmd);
 			this.spcList.Panel1.Controls.Add(this.tbcList);
 			// 
 			// spcList.Panel2
@@ -226,10 +240,24 @@
 			this.tbpListLog.Text = "ListLog";
 			this.tbpListLog.UseVisualStyleBackColor = true;
 			// 
+			// txtListLog
+			// 
+			this.txtListLog.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.txtListLog.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtListLog.Location = new System.Drawing.Point(3, 3);
+			this.txtListLog.Multiline = true;
+			this.txtListLog.Name = "txtListLog";
+			this.txtListLog.ReadOnly = true;
+			this.txtListLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtListLog.Size = new System.Drawing.Size(286, 226);
+			this.txtListLog.TabIndex = 1;
+			this.txtListLog.WordWrap = false;
+			// 
 			// tbcGrid
 			// 
 			this.tbcGrid.Controls.Add(this.tbpGrid);
 			this.tbcGrid.Controls.Add(this.tbpGridLog);
+			this.tbcGrid.Controls.Add(this.tbpLog);
 			this.tbcGrid.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tbcGrid.Location = new System.Drawing.Point(0, 0);
 			this.tbcGrid.Name = "tbcGrid";
@@ -258,27 +286,97 @@
 			this.tbpGridLog.Text = "GridLog";
 			this.tbpGridLog.UseVisualStyleBackColor = true;
 			// 
-			// txtListLog
-			// 
-			this.txtListLog.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.txtListLog.Location = new System.Drawing.Point(3, 3);
-			this.txtListLog.Multiline = true;
-			this.txtListLog.Name = "txtListLog";
-			this.txtListLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.txtListLog.Size = new System.Drawing.Size(286, 226);
-			this.txtListLog.TabIndex = 1;
-			this.txtListLog.WordWrap = false;
-			// 
 			// txtGridLog
 			// 
+			this.txtGridLog.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.txtGridLog.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.txtGridLog.Location = new System.Drawing.Point(3, 3);
 			this.txtGridLog.Multiline = true;
 			this.txtGridLog.Name = "txtGridLog";
+			this.txtGridLog.ReadOnly = true;
 			this.txtGridLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
 			this.txtGridLog.Size = new System.Drawing.Size(266, 226);
 			this.txtGridLog.TabIndex = 2;
 			this.txtGridLog.WordWrap = false;
+			// 
+			// tbpLog
+			// 
+			this.tbpLog.Controls.Add(this.txtLog);
+			this.tbpLog.Location = new System.Drawing.Point(4, 22);
+			this.tbpLog.Name = "tbpLog";
+			this.tbpLog.Padding = new System.Windows.Forms.Padding(3);
+			this.tbpLog.Size = new System.Drawing.Size(272, 232);
+			this.tbpLog.TabIndex = 2;
+			this.tbpLog.Text = "Log";
+			this.tbpLog.UseVisualStyleBackColor = true;
+			// 
+			// txtLog
+			// 
+			this.txtLog.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtLog.Location = new System.Drawing.Point(3, 3);
+			this.txtLog.Multiline = true;
+			this.txtLog.Name = "txtLog";
+			this.txtLog.ReadOnly = true;
+			this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtLog.Size = new System.Drawing.Size(266, 226);
+			this.txtLog.TabIndex = 3;
+			this.txtLog.WordWrap = false;
+			// 
+			// cboClickCmd
+			// 
+			this.cboClickCmd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cboClickCmd.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cboClickCmd.FormattingEnabled = true;
+			this.cboClickCmd.Items.AddRange(new object[] {
+            "(None)",
+            "select *",
+            "select count(*)"});
+			this.cboClickCmd.Location = new System.Drawing.Point(184, 0);
+			this.cboClickCmd.Name = "cboClickCmd";
+			this.cboClickCmd.Size = new System.Drawing.Size(114, 20);
+			this.cboClickCmd.TabIndex = 1;
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Location = new System.Drawing.Point(314, 1);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(64, 23);
+			this.btnOpen.TabIndex = 2;
+			this.btnOpen.Text = "&Open";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// btnClose
+			// 
+			this.btnClose.Enabled = false;
+			this.btnClose.Location = new System.Drawing.Point(384, 1);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(64, 23);
+			this.btnClose.TabIndex = 3;
+			this.btnClose.Text = "&Close";
+			this.btnClose.UseVisualStyleBackColor = true;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+			// 
+			// txtLimit
+			// 
+			this.txtLimit.Location = new System.Drawing.Point(454, 3);
+			this.txtLimit.Name = "txtLimit";
+			this.txtLimit.Size = new System.Drawing.Size(48, 21);
+			this.txtLimit.TabIndex = 4;
+			this.txtLimit.Text = "1000";
+			this.ttpMain.SetToolTip(this.txtLimit, "记录数限制. 若为0则表示不限制.");
+			// 
+			// btnExec
+			// 
+			this.btnExec.Enabled = false;
+			this.btnExec.Location = new System.Drawing.Point(508, 1);
+			this.btnExec.Name = "btnExec";
+			this.btnExec.Size = new System.Drawing.Size(64, 23);
+			this.btnExec.TabIndex = 5;
+			this.btnExec.Text = "&Exec";
+			this.btnExec.UseVisualStyleBackColor = true;
+			this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
 			// 
 			// ZDbViewCsForm
 			// 
@@ -289,17 +387,18 @@
 			this.Name = "ZDbViewCsForm";
 			this.Text = "zDbViewCs";
 			this.Load += new System.EventHandler(this.ZDbViewCsForm_Load);
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ZDbViewCsForm_FormClosed);
 			this.spcTool.Panel1.ResumeLayout(false);
 			this.spcTool.Panel2.ResumeLayout(false);
 			this.spcTool.ResumeLayout(false);
-			this.pnlTool.ResumeLayout(false);
-			this.pnlTool.PerformLayout();
-			this.mnuPopup.ResumeLayout(false);
 			this.tbcInput.ResumeLayout(false);
 			this.tbpConnstr.ResumeLayout(false);
 			this.tbpConnstr.PerformLayout();
 			this.tbpSql.ResumeLayout(false);
 			this.tbpSql.PerformLayout();
+			this.pnlTool.ResumeLayout(false);
+			this.pnlTool.PerformLayout();
+			this.mnuPopup.ResumeLayout(false);
 			this.spcList.Panel1.ResumeLayout(false);
 			this.spcList.Panel2.ResumeLayout(false);
 			this.spcList.ResumeLayout(false);
@@ -309,6 +408,8 @@
 			this.tbcGrid.ResumeLayout(false);
 			this.tbpGridLog.ResumeLayout(false);
 			this.tbpGridLog.PerformLayout();
+			this.tbpLog.ResumeLayout(false);
+			this.tbpLog.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -335,6 +436,14 @@
 		private System.Windows.Forms.TabPage tbpGridLog;
 		private System.Windows.Forms.TextBox txtListLog;
 		private System.Windows.Forms.TextBox txtGridLog;
+		private System.Windows.Forms.TabPage tbpLog;
+		private System.Windows.Forms.TextBox txtLog;
+		private System.Windows.Forms.ComboBox cboClickCmd;
+		private System.Windows.Forms.TextBox txtLimit;
+		private System.Windows.Forms.Button btnClose;
+		private System.Windows.Forms.Button btnOpen;
+		private System.Windows.Forms.Button btnExec;
+		private System.Windows.Forms.ToolTip ttpMain;
 	}
 }
 
--- zdbviewcs/ZDbViewCsForm.cs
+++ zdbviewcs/ZDbViewCsForm.cs
@@ -3,15 +3,68 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Common;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace zdbviewcs {
 	/// <summary>
 	/// zyl910's Database viewer (C#) (C#版数据库查看器).
 	/// </summary>
 	public partial class ZDbViewCsForm : Form {
+		/// <summary>
+		/// 当前数据库提供者.
+		/// </summary>
+		DbProviderFactory m_provider = null;
+
+		/// <summary>
+		/// 当前数据库连接.
+		/// </summary>
+		DbConnection m_conn = null;
+
+
+		/// <summary>
+		/// 将数据表转为xml.
+		/// </summary>
+		/// <param name="xmlDS"></param>
+		/// <returns></returns>
+		private string ConvertDataTableToXML(DataTable xmlDS) {
+			MemoryStream stream = null;
+			XmlTextWriter writer = null;
+			try {
+				stream = new MemoryStream();
+				writer = new XmlTextWriter(stream, Encoding.UTF8);
+				writer.Formatting = Formatting.Indented;
+				xmlDS.WriteXml(writer);
+				int count = (int)stream.Length;
+				byte[] arr = new byte[count];
+				stream.Seek(0, SeekOrigin.Begin);
+				stream.Read(arr, 0, count);
+				UTF8Encoding utf = new UTF8Encoding();
+				return utf.GetString(arr).Trim();
+			}
+			catch {
+				return String.Empty;
+			}
+			finally {
+				if (writer != null) writer.Close();
+			}
+		}
+
+		/// <summary>
+		/// 输出日志.
+		/// </summary>
+		/// <param name="s">日志文本.</param>
+		public void OutLog(string s) {
+			if (txtLog.Lines.Length > 3000) txtLog.Clear();
+			txtLog.Select(txtLog.Text.Length, 0);
+			txtLog.SelectedText = s + Environment.NewLine;
+			txtLog.Select(txtLog.Text.Length, 0);
+		}
+
 		public ZDbViewCsForm() {
 			InitializeComponent();
 		}
@@ -19,7 +72,7 @@ namespace zdbviewcs {
 		private void mnuPopupProviderRefresh_Click(object sender, EventArgs e) {
 			// 刷新提供者列表.
 			DataTable dt = DbProviderFactories.GetFactoryClasses();
-			//OutLog(ConvertDataTableToXML(dt));
+			OutLog(ConvertDataTableToXML(dt));
 			// cboFactory.
 			cboProvider.BeginUpdate();
 			cboProvider.Items.Clear();
@@ -35,7 +88,100 @@ namespace zdbviewcs {
 		}
 
 		private void ZDbViewCsForm_Load(object sender, EventArgs e) {
+			cboClickCmd.SelectedIndex = 1;
 			mnuPopupProviderRefresh_Click(mnuPopupProviderRefresh, null);
+		}
+
+		private void btnOpen_Click(object sender, EventArgs e) {
+			btnClose_Click(sender, null);
+			// main.
+			string sFactory = cboProvider.Text.Trim();
+			try {
+				m_provider = DbProviderFactories.GetFactory(sFactory);
+			}
+			catch (Exception ex) {
+				OutLog(ex.Message);
+				MessageBox.Show(this, ex.ToString(), this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			if (null == m_provider) return;
+			try {
+				// DbConnection.
+				try {
+					OutLog("Create DbConnection:");
+					m_conn = m_provider.CreateConnection();
+					OutLog(string.Format("\t{0}", m_conn.GetType().ToString()));
+				}
+				catch (Exception ex) {
+					OutLog(ex.Message);
+					MessageBox.Show(this, ex.ToString(), this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+					try {
+						m_conn.Dispose();
+					}
+					catch (Exception ex2) {
+						OutLog(ex2.ToString());
+					}
+					m_conn = null;
+					return;
+				}
+				if (null == m_conn) return;
+				// open.
+				try {
+					m_conn.ConnectionString = txtConnstr.Text.Trim();
+					m_conn.Open();
+				}
+				catch (Exception ex) {
+					OutLog(ex.Message);
+					m_conn.Dispose();
+					m_conn = null;
+					return;
+				}
+			}
+			finally {
+				if (null == m_conn) {
+					m_provider = null;
+				}
+			}
+			// done.
+			btnOpen.Enabled = false;
+			btnClose.Enabled = true;
+			btnExec.Enabled = true;
+			OutLog("Db opened.");
+		}
+
+		private void btnClose_Click(object sender, EventArgs e) {
+			m_provider = null;
+			if (null == m_conn) return;
+			try {
+				m_conn.Close();
+				m_conn.Dispose();
+			}
+			catch (Exception ex) {
+				OutLog(ex.ToString());
+			}
+			m_conn = null;
+			// done.
+			btnOpen.Enabled = true;
+			btnClose.Enabled = false;
+			btnExec.Enabled = false;
+			OutLog("Db closed.");
+		}
+
+		private void btnExec_Click(object sender, EventArgs e) {
+			//
+		}
+
+		private void ZDbViewCsForm_FormClosed(object sender, FormClosedEventArgs e) {
+			if (null != m_conn) {
+				try {
+					m_conn.Close();
+				}
+				catch (Exception ex) {
+					Trace.TraceError(ex.ToString());
+				}
+				m_conn = null;
+			}
+			m_provider = null;
 		}
 
 	}
